8250cde539c2c9948d0b8c0452b4fe9681a71449
calighula <montoya.miguel@gmail.com>
2015-01-24 11:33:15 +0100

    Project setup
    Project setups, initial assets and folders

---
+++ Assets/_Scripts/controller/UIController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour
+{
+
+		public GameController gameController;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void actionSelected (string action)
+		{
+				Debug.Log ("actionSelected: " + action);
+				gameController.move (action);
+
+				
+		}
+
+
+}

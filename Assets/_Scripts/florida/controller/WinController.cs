cadca2e111b7a858090abfe964224cb6f084eacc
calighula <montoya.miguel@gmail.com>
2015-01-25 05:27:43 +0100

    Game completed with Win action

---
+++ Assets/_Scripts/florida/controller/WinController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using florida.core;
+
+public class WinController : MonoBehaviour
+{
+
+    public float groundRadius;
+
+    public LayerMask whatIsPlayer; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 gameObjectLocation = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        bool isPlayerClose = Physics2D.OverlapCircle(gameObjectLocation, groundRadius, whatIsPlayer);
+
+        if (isPlayerClose)
+        {
+            GameCore.getInstance().loadScene("Win");
+        }
+    }
+}
